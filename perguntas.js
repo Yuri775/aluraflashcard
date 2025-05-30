e13017b8249ee904d67061bd8339e7152a66071d
Yuri775 <yuri.silvestrini@escola.pr.gov.br>
2025-05-30 21:36:32 +0000

    aula10

--- perguntas.js
+++ perguntas.js
@@ -21,3 +21,33 @@ criaCartao(
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
+
+criaCartao(
+    'Lingua portuguesa',
+    'O que é um verbo no infinitivo?',
+    'É quando o verbo tá na forma original: terminar em -ar, -er ou -ir, tipo "comer", "falar", "dormir".'
+)
+
+criaCartao(
+    'Matemática',
+    'Qual a fórmula da área do triângulo?',
+    'A = (base × altura) ÷ 2'
+)
+
+criaCartao(
+    'Física/química',
+    'O que é o ponto de ebulição da água?',
+    'É a temperatura que a água começa a ferver: 100°C no nível do mar.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual a função do coração no corpo humano?',
+    'Bombear o sangue pra circular oxigênio e nutrientes pelo corpo todo.'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'O que é sujeito na frase?',
+    'É quem faz a ação.'
+)
